98de60892ed600fff312c005d615f886c65056d7
EasternJamm <apetremarianioan@gmail.com>
2023-05-03 15:07:59 +0300

    2. Implementing pexels API

---
+++ app.js
@@ -0,0 +1,28 @@
+const auth = "563492ad6f917000010000018631d2490a644c00a8390f2557c9a8c8";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const submitButton = document.querySelector(".submit-btn");
+let searchValue;
+
+async function curatedPhotos() {
+  const dataFetch = await fetch(
+    "https://api.pexels.com/v1/curated?per_page=15&page=1",
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    }
+  );
+  const data = await dataFetch.json();
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src=${photo.src.large}></img>
+    <p>${photo.photographer}</p>
+    `;
+    gallery.appendChild(galleryImg);
+  });
+}
+curatedPhotos();
